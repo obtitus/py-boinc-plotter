379974fb540fa7d7580c6a3bcd94e6f6ef33b157
?ystein Bj?rndal <ob@cakebox.net>
2013-04-22 14:15:55 +0200

    version bump

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ class MyConfigParser(ConfigParser.ConfigParser):
             name = username
         keyring.set_password(section, name, password)
         
-__version_info__ = (0, 1, 2)
+__version_info__ = (0, 1, 3)
 __version__ = '.'.join(map(str, __version_info__))
 appName = 'BoincPlotter'
 appAuthor = 'obtitus'                   # [at] gmail.com
