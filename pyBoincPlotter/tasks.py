52821632fb7685de614c060f2dc283d941698cfb
?ystein Bj?rndal <ob@cakebox.net>
2013-06-17 12:52:22 +0200

    implementing task

--- pyBoincPlotter/tasks.py
+++ pyBoincPlotter/tasks.py
@@ -1,14 +1,33 @@
+"""
+Task -> Task_local
+Task -> Task_web -> Task_web_worlcommunitygrid
+"""
+
 # Standard python imports
 import datetime
 
-
 class Task(object):
+    """
+    Mostly handles string conversion around the following properties:
+    - name
+    - device
+    - state
+    - elapsedCPUtime
+    - remainingCPUtime
+    - deadline
+    - claimedCredit
+    - grantedCredit
+    Subclasses Task_local and Task_web are the one to use depending on source.
+    """
     desc_state = ['downloading', 'ready to run', 'running', 'suspended', 'paused', 'computation completed', 'uploading', 'ready to report', 'unknown']
+    fmt = '{0:<{col0}} {1:<{col1}} {2:<{col2}} {3:<{col3}} {4:<{col4}} {5:<{col5}} {6:<{col6}} {6:<{col6}} {7:<{col7}} {8:<{col8}}'
+    columnSpacing = dict()
+    for i in range(0, 8+1):
+        columnSpacing['col'+str(i)] = 0
 
     def __init__(self, name='', device='localhost',
                  state='unknown', fractionDone='0',
-                 elapsedCPUtime=None, remainingCPUtime=None, deadline=None,
-                 claimedCredit=None, grantedCredit=None):
+                 elapsedCPUtime=None, remainingCPUtime=None, deadline=None):
         """
         Each attribute has a string representation and a 'data' representation:
         type(self.fractionDone) == str
@@ -26,10 +45,15 @@ class Task(object):
         self.remainingCPUtime = remainingCPUtime # stored as timedelta, see strToTimedelta and timedeltaToStr
         self.deadline = deadline                 # stored as datetime, string is time until deadline
 
-        self.grantedCredit = grantedCredit # stored as float
-        self.claimedCredit = claimedCredit # stored as float
-
+    def __str__(self):
+        return self.fmt.format(self.nameShort,
+                               self.state, self.fractionDone,
+                               self.elapsedCPUtime, self.remainingCPUtime, self.deadline, 
+                               self.claimedCredit, self.grantedCredit, self.device, **self.columnSpacing)
+    
+    #
     # Conversion functions
+    #
     # Applied to self.elapsedCPUtime and self.remainingCPUtime
     def strToTimedelta(self, sec):
         timedelta = datetime.timedelta(seconds=float(sec))
@@ -42,12 +66,17 @@ class Task(object):
             timedelta = timedelta[:ix]
         return timedelta
 
+    #
+    # Properties
+    #
     @property
     def nameShort(self):
         if len(self.name) > 15:
             return self.name[:15] + '...'
         else:
             return self.name
+
+    # device - not needed
 
     @property
     def state(self):
@@ -73,8 +102,41 @@ class Task(object):
     def fractionDone(self, fractionDone):
         self._fractionDone = float(fractionDone)*100
 
+    @property
+    def elapsedCPUtime(self):
+        return self.timedeltaToStr(self._elapsedCPUtime)
+    @elapsedCPUtime.setter
+    def elapsedCPUtime(self, elapsedCPUtime):
+        self._elapsedCPUtime = self.strToTimedelta(elapsedCPUtime)
+
+    @property
+    def remainingCPUtime(self):
+        return self.timedeltaToStr(self._remainingCPUtime)
+    @remainingCPUtime.setter
+    def remainingCPUtime(self, remainingCPUtime):
+        self._remainingCPUtime = self.strToTimedelta(remainingCPUtime)
+
+    @property
+    def deadline(self):
+        """ Time until deadline
+        """
+        now = datetime.datetime.today()
+        delta = self._deadline - now
+        s = self.timedeltaToStr(delta)
+        if delta.days < 0:
+            delta = now - self._deadline
+            s = '-' + self.timedeltaToStr(delta)
+        return s
+    @deadline.setter
+    def deadline(self, deadline):
+        """ Store deadline as datetime object
+        """
+        self._deadline = datetime.datetime.strptime(deadline, self.fmt_date)
 
 class Task_local(Task):
+    fmt_date = '%a %b %d %H:%M:%S %Y'
+    desc_schedularState = ['ready to start', 'suspended', 'running']
+    desc_active = ['paused', 'running', 2, 3, 4, 5, 6, 7, 8, 'running']
     def __init__(self, schedularState, active, **kwargs):
         self.schedularState = schedularState
         self.active = active
@@ -101,3 +163,70 @@ class Task_local(Task):
         elif self.active in ['Paused', 'Running']:
             state = self.active
         return state
+
+    @property
+    def schedularState(self):
+        state = self.desc_schedularState[self._schedularState]
+        return state
+    @schedularState.setter
+    def schedularState(self, state):
+        self._schedularState = int(state)
+
+
+    @property
+    def active(self):
+        state = self.desc_active[self._active]
+        return state
+    @active.setter
+    def active(self, state):
+        self._active = int(state)
+
+class Task_web(Task):
+    fmt_date = '%d %b %Y %H:%M:%S UTC'
+
+    def __init__(self, claimedCredit='0', grantedCredit='0', **kwargs):
+        self.grantedCredit = grantedCredit # stored as float
+        self.claimedCredit = claimedCredit # stored as float
+        super(Task_web, self).__init__(**kwargs)
+
+    def toFloat(self, value):
+        value = value.replace(',', '')
+        value = value.replace('---', '0')
+        return float(value)
+
+    def done(self):
+        return not(self.desc_state[self._state] == 'in progress')
+
+    @property
+    def grantedCredit(self):
+        return str(self._grantedCredit)
+    @grantedCredit.setter
+    def grantedCredit(self, grantedCredit):
+        self._grantedCredit = self.toFloat(grantedCredit)
+
+    @property
+    def claimedCredit(self):
+        return str(self._claimedCredit)
+    @claimedCredit.setter
+    def claimedCredit(self, claimedCredit):
+        self._claimedCredit = self.toFloat(claimedCredit)
+
+    @Task.state.setter
+    def state(self, state):
+        if state.lower() == 'completed and validated':
+            state = 'valid'
+        elif state.lower() == 'over success done':
+            state = 'valid'
+        elif state.lower().endswith('waiting for validation'):
+            state = 'pending validation'
+        elif state.lower().endswith('validation inconclusive'):
+            state = 'inconclusive'
+        elif state.lower().endswith('invalid'):
+            state = 'invalid'
+        elif state.lower().startswith('in progress'):
+            state = 'in progress'
+        if 'error' in state.lower():
+            state = 'error'
+        super(Task_web, self).state.setter(state)
+class Task_web_worlcommunitygrid(Task_web):
+    fmt_date = '%m/%d/%y %H:%M:%S'
--- pyBoincPlotter/test/test_tasks.py
+++ pyBoincPlotter/test/test_tasks.py
@@ -4,13 +4,86 @@ import unittest
 # This project
 import tasks
 
-class TestTasks(unittest.TestCase):   
-    def setUp(self):
-        pass
-    
+class TestTasks_local(unittest.TestCase):   
+    def setUp(self, schedularState='0', active='0', name='foobar 123451234512345', device='this',
+              state='0', fractionDone='0',
+              elapsedCPUtime='60', remainingCPUtime='3600', deadline='Tue Jun 25 10:41:00 2013'):
+
+        self.t = tasks.Task_local(schedularState=schedularState, active=active, name=name, device=device,
+                                  state=state, fractionDone=fractionDone,
+                                  elapsedCPUtime=elapsedCPUtime, remainingCPUtime=remainingCPUtime, deadline=deadline)
+
     def test_name(self):
-        pass
+        self.assertEqual(self.t.name, 'foobar 123451234512345')
+        self.assertEqual(self.t.nameShort, 'foobar 12345123...')
+
+    def test_device(self):
+        self.assertEqual(self.t.device, 'this')
+    
+    def test_state(self):
+        self.assertEqual(self.t._schedularState, 0)
+        self.assertEqual(self.t._active, 0)
+        self.assertEqual(self.t._state, 0)
+        self.assertEqual(self.t.state, 'ready to run')
+
+    def test_states(self):
+        def test(wanted, **kwargs):
+            self.setUp(**kwargs)
+            self.assertEqual(self.t.state, wanted)
+        test('ready to run', state='2', schedularState='0', active='0')
+        test('ready to run', state='4', schedularState='0', active='0')
+        test('suspended', state='2', schedularState='1', active='9')
+        # TODO: add more
+
+    def test_fractionDone(self):
+        def test(wanted, **kwargs):
+            self.setUp(**kwargs)
+            self.assertEqual(self.t.fractionDone, wanted)
+
+        test('0 %')
+        test('100 %', remainingCPUtime='0')
+
+    def test_elapsedCPUtime(self):
+        self.assertEqual(self.t.elapsedCPUtime, '0:01:00')
+    
+    def test_remainingCPUtime(self):
+        self.assertEqual(self.t.remainingCPUtime, '1:00:00')
+    
+    # def test_deadline(self):
+    #     now = datetime.datetime.today()
+    #     delta = datetime.strptime('Tue Jun 25 10:41:00 2013', '%a %b %d %H:%M:%S %Y') - now
         
+
+class TestTasks_web(TestTasks_local):
+    def setUp(self, claimedCredit='0', grantedCredit='0',
+              name='foobar 123451234512345', device='this',
+              state='in progress', fractionDone='0',
+              elapsedCPUtime='60', remainingCPUtime='3600', deadline='25 Jun 2013 10:41:00 UTC'):
+
+        self.t = tasks.Task_web(claimedCredit=claimedCredit, grantedCredit=grantedCredit,
+                                name=name, device=device,
+                                state=state, fractionDone=fractionDone,
+                                elapsedCPUtime=elapsedCPUtime, remainingCPUtime=remainingCPUtime, deadline=deadline)
+
+    def test_state(self):
+        self.assertEqual(self.t._state, 9)
+        self.assertEqual(self.t.state, 'in progress')
+
+    def test_states(self):
+        def test(wanted, **kwargs):
+            self.setUp(**kwargs)
+            self.assertEqual(self.t.state, wanted)
+        test('valid', state='Completed and validated')
+
+    def test_fractionDone(self):
+        def test(wanted, **kwargs):
+            self.setUp(**kwargs)
+            self.assertEqual(self.t.fractionDone, wanted)
+
+        test('0 %')
+        test('100 %', state='completed and validated')
+
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestTasks)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    for t in [TestTasks_local, TestTasks_web]:
+        suite = unittest.TestLoader().loadTestsFromTestCase(t)
+        unittest.TextTestRunner(verbosity=2).run(suite)
