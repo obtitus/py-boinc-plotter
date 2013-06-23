073faf9b327df209caa3d83fb803cb823081079f
?ystein Bj?rndal <ob@cakebox.net>
2013-06-23 11:24:32 +0200

    working on statistics for project

--- pyBoincPlotter/project.py
+++ pyBoincPlotter/project.py
@@ -1,19 +1,13 @@
 # Standard python
 import re
-# Non standard
-# Project imports
-from plot.jobLog import JobLog
 
 class Project(object):
-    def __init__(self, url, userid, password, username):
+    def __init__(self, url, user=None, statistics=None, settings=None):
         self.name = url
-        self.user = dict(userid=userid, password=password, username=username)
+        self.user = user
         self.applications = dict()
-        self.update()
-        
-    def update(self):
-        self.statistics_jobLog = self.get_jobLog()
-        self.statistics_ = None
+        self.statistics = statistics
+        self.settings = settings
 
     @property
     def name(self):
@@ -25,32 +19,13 @@ class Project(object):
         self.name_short = self.name_short.replace('http://', '')
         self.name_short = self.name_short.replace('www.', '')
         self.name_short = self.name_short.replace('.org', '')
+        if self.name_short[-1] == '/':
+            self.name_short = self.name_short[:-1]
 
-    def get_jobLog(self):
-        filename = None
-        with open(filename, 'r') as f:
-            data = self.parse_jobLog(f)
-        return data
-    
-    def parse_jobLog(self, f):
-        """
-        ue - estimated_runtime_uncorrected
-        ct - final_cpu_time, cpu time to finish
-        fe - rsc_fpops_est, estimated flops
-        et - final_elapsed_time, clock time to finish
-        """
-        data = JobLog()
-        now = datetime.datetime.now()
-        for line in f:
-            s = line.split()
-            assert len(s) == 11, 'Line in job log not recognized {0} "{1}" -> "{2}"'.format(len(s), line, s)
-            t = int(s[0])
-            t = datetime.datetime.fromtimestamp(t)
-            if limitDaysToPlot == None or now - t < limitDaysToPlot:
-                data.time.append(t)
-                data.ue.append(float(s[2]))
-                data.ct.append(float(s[4]))
-                data.fe.append(float(s[6]))
-                data.names.append(s[8])
-                data.et.append(float(s[10]))
-        return data
+    def __str__(self):
+        endl = '\n'
+        ret = []
+        for prop in [self.name_short, self.settings, self.statistics]:
+            if prop != None:
+                ret.append(str(prop))
+        return "\n".join(ret)
--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
             "matplotlib",
             "numpy",
             "PIL",
+            "lxml",
             "beautifulsoup4",
             "appdirs"
         ],
