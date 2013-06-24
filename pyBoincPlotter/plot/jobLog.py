17d1d4a156ee4ea5bbe01d80c1906bf12715aedc
?ystein Bj?rndal <ob@cakebox.net>
2013-06-24 15:10:13 +0200

    moved job_log to plot, not tested

--- pyBoincPlotter/plot/jobLog.py
+++ pyBoincPlotter/plot/jobLog.py
@@ -5,13 +5,51 @@ from collections import namedtuple
 from importMatplotlib import *
 
 class JobLog(namedtuple('JobLog', dict(time=list(), ue=list(), ct=list(), fe=list(), et=list(), names=list()))):
-    def plot(self):
-        self.N = 4                               # Number of subplots
-        time, ue, ct, fe, name, et = plt.date2num(self.time), self.ue, self.ct, self.fe, self.names, self.et
-        self.plot_datapoints(time, ue, ct, fe, name, et)
-        self.plot_hist_day(time, ue, ct, fe, name, et)
+    @staticmethod
+    def createFromFilename(filename):
+        """ Returns a JobLog instance from a given filename
+        """
+        with open(filename, 'r') as f:
+            return self.createFromFilehandle(f)
 
-    def plot_datapoints(self, time, ue, ct, fe, name, et):
+    @staticmethod
+    def createFromFilehandle(f):
+        """ Returns a JobLog instance from a given filehandle
+        ue - estimated_runtime_uncorrected
+        ct - final_cpu_time, cpu time to finish
+        fe - rsc_fpops_est, estimated flops
+        et - final_elapsed_time, clock time to finish
+        """
+        data = JobLog()
+        now = datetime.datetime.now()
+        for line in f:
+            s = line.split()
+            assert len(s) == 11, 'Line in job log not recognized {0} "{1}" -> "{2}"'.format(len(s), line, s)
+            t = int(s[0])
+            t = datetime.datetime.fromtimestamp(t)
+            if limitDaysToPlot == None or now - t < limitDaysToPlot:
+                data.time.append(plt.date2num(t))
+                data.ue.append(float(s[2]))
+                data.ct.append(float(s[4]))
+                data.fe.append(float(s[6]))
+                data.names.append(s[8])
+                data.et.append(float(s[10]))
+        return data
+
+    def plot(self, fig):
+        """ Plots the job_log to the given figure instance
+        """
+        self.N = 4                               # Number of subplots
+
+        # Plot datapoints and bars, make sure the same colors are used.
+        color = self.plot_datapoints(fig)
+        self.plot_hist_day(color, fig)
+
+    def plot_datapoints(self, fig):
+        """
+        Let each datapoint be a single dot
+        """
+        time, ue, ct, fe, name, et = self.time, self.ue, self.ct, self.fe, self.names, self.et
         ix = 1                              # current subplot
         kwargs = dict(ls='none', marker='o')
 
@@ -41,8 +79,14 @@ class JobLog(namedtuple('JobLog', dict(time=list(), ue=list(), ct=list(), fe=lis
             plt.setp(ax.get_xticklabels(), visible=False)
             ax1.yaxis.set_major_formatter(formatter_timedelta)
 
-    def plot_hist_day(self, time, ue, ct, fe, name, et):
-        # Now for the histogram part
+        return color
+
+    def plot_hist_day(self, color, fig):
+        """
+        Create a single bar for each day
+        """
+        time, ue, ct, fe, name, et = self.time, self.ue, self.ct, self.fe, self.names, self.et
+
         if len(time) == 0: return ;         # make sure we actually have data to work with
 
         currentDay = plt.num2date(time[0])
